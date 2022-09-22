8330ae3a5918b5a63ccc293ed42e4aa71325e7d0
kumasumit <kumarsumit1989@gmail.com>
2022-09-22 15:40:19 +0530

    Add middleware to fetch user from a valid token

--- Backend/middlewares/fetch_user_middleware.js
+++ Backend/middlewares/fetch_user_middleware.js
@@ -1,8 +1,23 @@
 var jwt = require('jsonwebtoken');
-
+const JWT_TOKEN = "sumitisagoo$dby";
 const fetchUser = function (req, res, next){
-
+//get the user from the jwt token and add id to the req object
+const token = req.header('auth-token');
+if(!token){
+    //if there is no token
+    res.status(401).send({error: "Please authenticate using valid token"});
+}
+try{
+//if token exists, verify and get the user from jwt.verify
+const data = jwt.verify(token, JWT_TOKEN);
+//copy the verified user into the req.user
+req.user = data.user;
  next();
+}catch(error){
+    console.log(error);
+    res.status(401).send({error: "Please authenticate using valid token"});
+}
+
 }
 
 module.exports = fetchUser;
