4d4532f848be3e899831f65988ffea89abb50461
Kumar Sumit <kumarsumit1989@gmail.com>
2022-11-05 00:28:07 +0530

    configure navbar and react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
-      <h1>This is INotes FrontEnd React App</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/* here we wrap our entire app in browserRouter so that our app can use routes */}
     <App />
   </React.StrictMode>
 );
