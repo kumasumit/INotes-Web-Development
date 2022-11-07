851130a6392109828f3a6ce7f2912b1460c18090
Kumar Sumit <kumarsumit1989@gmail.com>
2022-11-07 23:42:28 +0530

    Modify Alert component

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,10 +6,17 @@ const Alert = (props) => {
     return lower.charAt(0).toUpperCase() + lower.slice(1);
   };
   return (
+    // <div style={{height: '50px'}>
+    //   {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+    //  <strong>
+    //    {capitalize(props.alert.type)}
+    //  </strong>:{props.alert.message}
+    //   </div>}
+    // </div>
     <div style={{ height: "50px" }}>
       {props.alert && (
         <div
-          className="alert alert-${props.alert.type} alert-dismissible fade show"
+          className={`alert alert-${props.alert.type} alert-dismissible fade show`}
           role="alert"
         >
           <strong>{capitalize(props.alert.type)}</strong> : {props.alert.msg}
